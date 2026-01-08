c520d51f9dc285a5300634d1399d7968df540bdb
dflorian251 <dflorian251@gmail.com>
2026-01-08 18:53:43 +0200

    Updating constants.

--- constants/theme.ts
+++ constants/theme.ts
@@ -3,7 +3,7 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-import { Platform } from 'react-native';
+import { Platform, useColorScheme } from 'react-native';
 
 const tintColorLight = '#ff6b35';
 const tintColorDark = '#ff6b35';
@@ -11,6 +11,10 @@ const tintColorDark = '#ff6b35';
 export const MAX_REVIEWS_PREVIEW = 3;
 export const BRAND_BLUE = "#4A90E2";
 export const BRAND_PURPLE = "#8b5cf6";
+
+export const colorScheme = useColorScheme() ?? "light";
+export const isDark = colorScheme === 'dark';
+
 
 export const Colors = {
   light: {
